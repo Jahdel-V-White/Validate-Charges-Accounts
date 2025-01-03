d9dee635c4efeeb114f7c1fb0a6f440af734c1a3
Jahdel White <166260123+Jahdel-V-White@users.noreply.github.com>
2025-01-03 12:27:40 -0500

    Add files via upload

---
+++ White_Ch7.3/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace White_Ch7._3
+{
+    partial class ChargeAccValidation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.ValidateBtn = new System.Windows.Forms.Button();
+            this.ExitBtn = new System.Windows.Forms.Button();
+            this.UserInput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(250, 205);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(750, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter the 7 digit number of the Charge Account you are seaching for in the Box be" +
+    "low.\r\n";
+            // 
+            // ValidateBtn
+            // 
+            this.ValidateBtn.Location = new System.Drawing.Point(325, 408);
+            this.ValidateBtn.Name = "ValidateBtn";
+            this.ValidateBtn.Size = new System.Drawing.Size(162, 54);
+            this.ValidateBtn.TabIndex = 4;
+            this.ValidateBtn.Text = "Click to Validate Account #";
+            this.ValidateBtn.UseVisualStyleBackColor = true;
+            this.ValidateBtn.Click += new System.EventHandler(this.ValidateBtn_Click);
+            // 
+            // ExitBtn
+            // 
+            this.ExitBtn.Location = new System.Drawing.Point(733, 408);
+            this.ExitBtn.Name = "ExitBtn";
+            this.ExitBtn.Size = new System.Drawing.Size(162, 54);
+            this.ExitBtn.TabIndex = 5;
+            this.ExitBtn.Text = "Exit";
+            this.ExitBtn.UseVisualStyleBackColor = true;
+            // 
+            // UserInput
+            // 
+            this.UserInput.Location = new System.Drawing.Point(512, 315);
+            this.UserInput.Name = "UserInput";
+            this.UserInput.Size = new System.Drawing.Size(191, 26);
+            this.UserInput.TabIndex = 6;
+            this.UserInput.TextChanged += new System.EventHandler(this.UserInput_TextChanged);
+            this.UserInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.UserInput_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(427, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(385, 37);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Charge Account Validator";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 102);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(232, 326);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+          //  this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // ChargeAccValidation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1204, 628);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.UserInput);
+            this.Controls.Add(this.ExitBtn);
+            this.Controls.Add(this.ValidateBtn);
+            this.Controls.Add(this.label1);
+            this.Name = "ChargeAccValidation";
+            this.Text = "Charge Account Validation";
+            this.Load += new System.EventHandler(this.ChargeAccValidation_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ValidateBtn;
+        private System.Windows.Forms.Button ExitBtn;
+        private System.Windows.Forms.TextBox UserInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ White_Ch7.3/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace White_Ch7._3
+{
+    public partial class ChargeAccValidation : Form
+    {
+        public ChargeAccValidation()
+        {
+            InitializeComponent();
+        }
+
+        private void ChargeAccValidation_Load(object sender, EventArgs e)
+        {
+
+        }
+        int UserGuess;
+
+        //Ensure tha the user can only enter digits
+        private void UserInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+            
+            
+            //Turn user input into an integer that can be used for calculation
+            if (int.TryParse(UserInput.Text, out UserGuess))
+            {
+
+            }
+
+            
+        }
+
+        private void ValidateBtn_Click(object sender, EventArgs e)
+        {
+            //try catch is done to catch any errors that may occur with the file itself
+            try
+            {
+                //creating the size of the array I know it will hold 18 elements
+                
+                int[] nums = new int[19];
+
+                //create index
+                int count = 0;
+
+                //file will be read to array
+                StreamReader inputFile;
+
+                //file opened
+                inputFile = File.OpenText("ChargeAccounts.txt");
+
+                //loop adds each new line from the file as a new element to the array until the end of the file
+                while (count < nums.Length && !inputFile.EndOfStream)
+                {
+                    //new element created for evert new line in the file
+                    nums[count] = int.Parse(inputFile.ReadLine());
+                    count++;
+                }
+                inputFile.Close();
+
+                 
+                
+                //loop that parses through each line that has been read into the array
+                foreach (int num in nums)
+                {
+
+                    //if statement will check the user input up against each element in the array
+                    //the count representing each element
+                    {
+                        if (UserGuess == nums[count])
+                        {
+                            MessageBox.Show("You Have the Correct Charge Number.");
+                            //the found variable becomes true because the user has guess correct
+                            
+
+                            //the break statement was what I had to add or else the user had to click ok 19 times to exit the message
+                            //the code was making the user click as many times as there are elements in the array the break stops this
+                            break;
+                        }
+
+                        
+                        else if (UserGuess != nums[count])
+                        {
+                            MessageBox.Show("Your Answer is wrong click Ok Button to try again");
+                        }
+                        break;
+                    } 
+
+                }
+
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("FILE ERROR: " + ex.Message);
+            }
+
+            
+
+        }
+
+        private void UserInput_TextChanged(object sender, EventArgs e)
+        {
+            int maxDigits = 7;
+            //ensure that the user cannot enter any digits that exceed 6 digits as all the charge numbers are 6 digits
+            if (UserInput.Text.Length > maxDigits)
+            {
+                MessageBox.Show("Cannot Enter a number that exceeds the 7 digits try again.");
+            }
+
+        }
+
+        
+
+    }
+}
